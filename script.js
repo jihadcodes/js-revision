c6a522ca96d803fb7b6902c40b463f4f2e917e7e
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-08 11:44:28 +0600

    for lop

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 
  let  x = 40; 
-  let y = 30 ;
- function logical() {
-    if (x > y) {
-        console.log("yeah, This is true !");
+//   let y = 30 ;
+//  function logical() {
+//     if (x > y) {
+//         console.log("yeah, This is true !");
         
-    }
-    else{
-        console.log( "False");
+//     }
+//     else{
+//         console.log( "False");
         
-    }
+//     }
     
- }
- logical();
+//  }
+//  logical();
 
- function logical() {
-    {
-       x > y ? console.log("yeah, This is true !") : console.log( "Wrong");
+//  function logical() {
+//     {
+//        x > y ? console.log("yeah, This is true !") : console.log( "Wrong");
         
-    }
+//     }
     
- }
- logical();
+//  }
+//  logical();
 
 
  let num1 = 20;
  let num2 = 30 ; 
  let  num3 = 50 ;
- console.log(num1<num2  && num2 > num3);
- console.log(num1<num2  && num2 < num3);
+//  console.log(num1<num2  && num2 > num3);
+//  console.log(num1<num2  && num2 < num3);
+
+//  console.log(num1<num2  || num2 < num3);
  
+// console.log(!(num1<num2));
+
+for (let x =1; x <=10; x++ ) {
+    console.log('I Love to code ');
+    
+}
+
+
