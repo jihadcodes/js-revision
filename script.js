ae85376222974ad076b7d163ee86fc86ea0a3deb
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-08 11:04:07 +0600

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+
+ let  x = 40; 
+  let y = 30 ;
+ function logical() {
+    if (x > y) {
+        console.log("yeah, This is true !");
+        
+    }
+    else{
+        console.log( "False");
+        
+    }
+    
+ }
+ logical();
+
+ function logical() {
+    {
+       x > y ? console.log("yeah, This is true !") : console.log( "Wrong");
+        
+    }
+    
+ }
+ logical();
+
